3e44bdf4761830506b899c8e605acca3c7524bec
alyalsayed <alyalsayed56@yahoo.com>
2024-09-21 16:21:47 +0300

    update portfolio

--- Task2-portfolio/js/main.js
+++ Task2-portfolio/js/main.js
@@ -1,7 +1,5 @@
 const typedTextElement = document.querySelector(".typed-text-output");
-
 const textToType = document.querySelector(".typed-text").textContent;
-
 const parts = textToType.split(",").map((part) => part.trim());
 
 let index = 0;
@@ -16,7 +14,7 @@ const typeText = () => {
     );
     if (typedTextElement.textContent === parts[index]) {
       isTyping = false;
-      setTimeout(deleteText, 100);
+      setTimeout(deleteText, 1000); // Pause before deleting
     } else {
       setTimeout(typeText, 100);
     }
@@ -32,13 +30,39 @@ const deleteText = () => {
     );
     setTimeout(deleteText, 100);
   } else {
-    index++;
-    if (index < parts.length) {
-      isTyping = true;
-      setTimeout(typeText, 50);
-    }
+    isTyping = true;
+    index = (index + 1) % parts.length; 
+    setTimeout(typeText, 50);
   }
 };
 
 // Start typing
 setTimeout(typeText, 100);
+
+fetch('js/projects.json')
+.then(response => response.json())
+.then(data => {
+    const projectContainer = document.querySelector('#projects .row');
+    data.forEach(project => {
+        const projectCard = document.getElementById('project-template').cloneNode(true);
+        projectCard.style.display = 'block';
+        projectCard.querySelector('.card-img-top').src = project.image;
+        projectCard.querySelector('.card-title').textContent = project.title;
+        projectCard.querySelector('.card-text').textContent = project.description;
+
+         // Check if live link exists; if not, use GitHub link
+         const liveLinkBtn = projectCard.querySelector('.live-link');
+         if (project.live_link && project.live_link.trim() !== "") {
+             liveLinkBtn.href = project.live_link;
+             liveLinkBtn.style.display = 'inline-block'; // Show button if live link exists
+         } else {
+             liveLinkBtn.href = project.github_link; // Use GitHub link instead
+             liveLinkBtn.style.display = 'inline-block'; // Show button
+         }
+
+        projectCard.querySelector('.btn-outline-secondary').href = project.github_link;
+        projectCard.querySelector('.text-muted').textContent = project.skills.join(', ');
+        projectContainer.appendChild(projectCard);
+    });
+})
+.catch(error => console.error('Error fetching the projects:', error));
